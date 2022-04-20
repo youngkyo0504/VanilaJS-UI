1d6338084c687827c4f24e3347fa6376aa7380e6
keumk <keumky2@gmail.com>
2022-04-21 00:55:03 +0900

    :sparkles: carousel 완성..!

--- 1-1 Carousel/index.js
+++ 1-1 Carousel/index.js
@@ -1,48 +1,73 @@
 class Carousel {
-    constructor() {
-        this.carouselImages = document.querySelectorAll(".carousel-image")
-        this.activeIndex = 0;
-        this.prevBtn = document.querySelector('.prev-btn')
-        this.nextBtn = document.querySelector('.next-btn')
-        this.direction = 'right'
-    }
+  constructor() {
+    this.carouselImages = document.querySelectorAll(".carousel-image");
+    this.activeIndex = 0;
+    this.prevBtn = document.querySelector(".prev-btn");
+    this.nextBtn = document.querySelector(".next-btn");
+    this.tabItems = document.querySelectorAll(".gray-circle");
+    this.direction = "right";
+    this.iaAnimationOn = false;
+  }
 
-    init() {
-        this.addEventListenerAtBtns();
-        this.setImageState();
+  init() {
+    this.addEventListenerAtBtns();
+    this.addImageTransitionEndEvent();
+    this.updateState();
+  }
+  setIsAnimationOn(boolean) {
+    this.iaAnimationOn = boolean;
+  }
+  updateState() {
+    console.log({ index: this.activeIndex });
+    this.carouselImages[this.activeIndex].classList.add("active");
+    !this.isLastIndex() &&
+      this.carouselImages[this.activeIndex + 1].classList.add("next");
+    !this.isFirstIndex() &&
+      this.carouselImages[this.activeIndex - 1].classList.add("prev");
+    this.tabItems[this.activeIndex].classList.add("active-tab");
+  }
+  addImageTransitionEndEvent() {
+    for (const image of this.carouselImages) {
+      image.addEventListener("transitionend", (e) => {
+        if (e.propertyName === "transform") {
+          this.setIsAnimationOn(false);
+        }
+      });
     }
+  }
+  initializeState() {
+    this.carouselImages[this.activeIndex].classList.remove("active");
+    this.carouselImages[0].classList.remove("initial");
+    this.tabItems[this.activeIndex].classList.remove("active-tab");
+    !this.isLastIndex() &&
+      this.carouselImages[this.activeIndex + 1].classList.remove("next");
+    !this.isFirstIndex() &&
+      this.carouselImages[this.activeIndex - 1].classList.remove("prev");
+  }
+  addEventListenerAtBtns() {
+    const setActiveIndex = (direction) => () => {
+      if (
+        this.iaAnimationOn ||
+        (direction === "left" && this.isFirstIndex()) ||
+        (direction === "right" && this.isLastIndex())
+      )
+        return;
+      this.setIsAnimationOn(true);
+      this.initializeState();
+      this.activeIndex = this.activeIndex + (direction === "left" ? -1 : 1);
+      this.updateState();
+    };
+    this.prevBtn.addEventListener("click", setActiveIndex("left"));
+    this.nextBtn.addEventListener("click", setActiveIndex("right"));
+  }
 
-    setImageState() {
-        console.log({ "index": this.activeIndex })
-        this.carouselImages[this.activeIndex].classList.add('active');
-        !this.isLastIndex() && this.carouselImages[this.activeIndex + 1].classList.add('next')
-        !this.isFirstIndex() && this.carouselImages[this.activeIndex - 1].classList.add('prev')
-    }
-
-    initializeState() {
-        this.carouselImages[this.activeIndex].classList.remove('active');
-        this.carouselImages[0].classList.remove('initial');
-        !this.isLastIndex() && this.carouselImages[this.activeIndex + 1].classList.remove('next')
-        !this.isFirstIndex() && this.carouselImages[this.activeIndex - 1].classList.remove('prev')
-    }
-    addEventListenerAtBtns() {
-        const setActiveIndex = (change) => (() => {
-            if ((change < 0 && this.isFirstIndex()) || (change > 0 && this.isLastIndex())) return;
-            this.initializeState()
-            this.activeIndex = this.activeIndex + change;
-            this.setImageState()
-        })
-        this.prevBtn.addEventListener('click', setActiveIndex(-1));
-        this.nextBtn.addEventListener('click', setActiveIndex(1));
-    }
-
-    isFirstIndex() {
-        return this.activeIndex === 0;
-    }
-    isLastIndex() {
-        return this.activeIndex === this.carouselImages.length - 1;
-    }
+  isFirstIndex() {
+    return this.activeIndex === 0;
+  }
+  isLastIndex() {
+    return this.activeIndex === this.carouselImages.length - 1;
+  }
 }
 
-const carousel = new Carousel()
-carousel.init()
+const carousel = new Carousel();
+carousel.init();
